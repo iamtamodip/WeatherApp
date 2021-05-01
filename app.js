130a28f94d0394fcc1f0c2bf8804e161c07c652d
Tamodip Das <iamtamodip@hotmail.com>
2021-05-01 16:36:34 +0530

    Deploying

--- app.js
+++ app.js
@@ -1,46 +1,60 @@
-const express = require("express")
-const https= require("https")
-const bodyParser = require("body-parser")
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
 
-const app= express()
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req,res){
-    res.sendFile(__dirname + "/index.html")
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.post("/", function(req, res){
-    console.log(req.body.cityName)
+app.post("/", function (req, res) {
+  console.log(req.body.cityName);
 
-    const query = req.body.cityName
-    const apiKey = "1e0eca55b2dfbb2379a1529a1410eff2"
-    const unit = "metric"
-    const url="https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
-    
-    https.get(url, function(response){
-        console.log(response.statusCode);
-    
-        response.on("data", function(data){
-            // const weatherData = JSON.parse(data)
-            // console.log(weatherData)
+  const query = req.body.cityName;
+  const apiKey = "1e0eca55b2dfbb2379a1529a1410eff2";
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
 
-            const weatherData = JSON.parse(data)
-            const temp =  weatherData.main.temp
-            const weatherDescription = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+  https.get(url, function (response) {
+    console.log(response.statusCode);
 
-            res.write("<p>The weather in "+ query+ " is " + weatherDescription + ".<p>")
-            res.write("<h1>The temparature in "+ query+ " is " + temp + " degree Celcious. </h1>")
-            res.write("<img src=" + imageURL + ">")
-            res.send()
-        })
-    })
+    response.on("data", function (data) {
+      // const weatherData = JSON.parse(data)
+      // console.log(weatherData)
 
-    //Response.send("<h1>Server is up and running</h1>")
-})
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-app.listen(3000,function(){
-    console.log("Server running on port 3000")
-})
+      res.write(
+        "<p>The weather in " + query + " is " + weatherDescription + ".<p>"
+      );
+      res.write(
+        "<h1>The temparature in " +
+          query +
+          " is " +
+          temp +
+          " degree Celcious. </h1>"
+      );
+      res.write("<img src=" + imageURL + ">");
+      res.send();
+    });
+  });
+
+  //Response.send("<h1>Server is up and running</h1>")
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server running on port 3000");
+});
